8ca5f0d1af006ea2d1059779725b8b59dc249897
UltraLuke <42553418+UltraLuke@users.noreply.github.com>
2021-09-23 20:45:36 -0300

    character selection
    Se desarrolla la pantalla de seleccion de personajes

---
+++ Assets/Scripts/Multiplayer components/CharacterSelectionSetter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class CharacterSelectionSetter : MonoBehaviourPunCallbacks
+{
+    public PlayerSelectionUI[] charSelectors;
+    public Button readyBtn, notReadyBtn;
+
+    int _playersReady;
+    int _maxPlayers = 2;
+
+    public void OnEnable()
+    {
+        _playersReady = 0;
+        readyBtn.gameObject.SetActive(true);
+        notReadyBtn.gameObject.SetActive(false);
+
+        int index;
+        if (PhotonNetwork.IsMasterClient)
+            index = 0;
+        else
+            index = 1;
+
+        //for (int i = 0; i < charSelectors.Length; i++)
+        //{
+        //    if (i == index)
+        //        charSelectors[i].StartWithCharacter(index);
+        //}
+
+        for (int i = 0; i < charSelectors.Length; i++)
+        {
+            if(i == index)
+            {
+                charSelectors[index].StartWithCharacter(index, PlayerReady);
+            }
+            else
+            {
+                charSelectors[i].DisableSelector(PlayerReady);
+            }
+        }
+    }
+
+    void PlayerReady(bool ready)
+    {
+        if (!ready)
+        {
+            _playersReady = _playersReady <= 0 ? 0 : _playersReady - 1;
+            return;
+        }
+
+        _playersReady++;
+        if(_playersReady == _maxPlayers && PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Empezar el juego");
+            //EMPEZAR JUEGO
+        }
+    }
+}
--- Assets/Scripts/Multiplayer components/PlayerSelectionUI.cs
+++ Assets/Scripts/Multiplayer components/PlayerSelectionUI.cs
@@ -2,34 +2,154 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using System;
 
 public class PlayerSelectionUI : MonoBehaviourPunCallbacks
 {
-    public GameObject onlineLobbyScreen;
-    public GameObject playerSelectionUIScreen;
+    public GameObject[] characters;
+    public GameObject leftArrow, rightArrow;
+    public GameObject youIndicator;
+    public GameObject checkMark;
 
-    //bool _selfMade;
+    int _currentIndex;
+    int _maxIndex;
+    bool _updateUI = false;
+
+    bool _myCharacter;
+    bool _isReady;
+
+    Action<bool> _readySetter;
+
+    public bool IsReady { get => _isReady; }
+
+    private void Update()
+    {
+        if (_updateUI && _myCharacter)
+        {
+            photonView.RPC("RPCUpdateCharacter", RpcTarget.AllBuffered, _currentIndex);
+            photonView.RPC("RPCReady", RpcTarget.AllBuffered, false);
+            _updateUI = false;
+        }
+    }
+    public void StartWithCharacter(int character, Action<bool> readySetter)
+    {
+        _maxIndex = characters.Length - 1;
+        _currentIndex = character;
+        //checkMark.SetActive(false);
+
+        Debug.Log(gameObject.name);
+        Debug.Log(_currentIndex);
+
+        //if (photonView.IsMine)
+        //{
+        //_currentIndex = 0;
+        //characters[0].SetActive(true);
+        //characters[1].SetActive(false);
+        leftArrow.SetActive(true);
+        rightArrow.SetActive(true);
+        youIndicator.SetActive(true);
+        //}
+        //else
+        //{
+        //_currentIndex = 1;
+        //characters[1].SetActive(true);
+        //characters[0].SetActive(false);
+        //    leftArrow.SetActive(false);
+        //    rightArrow.SetActive(false);
+        //    youIndicator.SetActive(false);
+        //}
+
+        //photonView.RPC("RPCUpdateCharacter", RpcTarget.OthersBuffered, _currentIndex);
+        //RPCUpdateCharacter();
+        //photonView.RPC("RPCUpdateCharacter", RpcTarget.OthersBuffered);
+        _updateUI = true;
+        _myCharacter = true;
+        _readySetter = readySetter;
+    }
+
+    public void DisableSelector(Action<bool> readySetter)
+    {
+        leftArrow.SetActive(false);
+        rightArrow.SetActive(false);
+        youIndicator.SetActive(false);
+        //checkMark.SetActive(false);
+        _myCharacter = false;
+        _readySetter = readySetter;
+    }
+
+    public void SetReady(bool ready)
+    {
+        if (_myCharacter)
+        {
+            photonView.RPC("RPCReady", RpcTarget.All, ready);
+        }
+    }
 
     //private void Start()
     //{
-    //    _selfMade = false;
-    //}
 
-    //public void BtnLeftRoom()
-    //{
-    //    _selfMade = true;
-    //    PhotonNetwork.LeaveRoom();
-    //}
-
-    //public override void OnLeftRoom()
-    //{
-    //    Debug.Log("Room left");
-
-    //    if (_selfMade)
-    //        onlineLobbyScreen.SetActive(true);
+    //    if (photonView.IsMine)
+    //    {
+    //        _currentIndex = 0;
+    //        characters[0].SetActive(true);
+    //        characters[1].SetActive(false);
+    //        leftArrow.SetActive(true);
+    //        rightArrow.SetActive(true);
+    //        youIndicator.SetActive(true);
+    //    }
     //    else
-    //        disconnectedFromRoom.SetActive(true);
-
-    //    playerSelectionUIScreen.SetActive(false);
+    //    {
+    //        _currentIndex = 1;
+    //        characters[1].SetActive(true);
+    //        characters[0].SetActive(false);
+    //        leftArrow.SetActive(false);
+    //        rightArrow.SetActive(false);
+    //        youIndicator.SetActive(false);
+    //    }
     //}
+
+    public void BtnLeft()
+    {
+        if (_currentIndex == 0)
+            _currentIndex = _maxIndex;
+        else
+            _currentIndex--;
+
+        //RPCUpdateCharacter();
+        photonView.RPC("RPCUpdateCharacter", RpcTarget.AllBuffered, _currentIndex);
+        //photonView.RPC("RPCUpdateCharacter", RpcTarget.OthersBuffered, _currentIndex);
+    }
+
+    public void BtnRight()
+    {
+        if (_currentIndex == _maxIndex)
+            _currentIndex = 0;
+        else
+            _currentIndex++;
+
+        //RPCUpdateCharacter();
+        //photonView.RPC("RPCUpdateCharacter", RpcTarget.OthersBuffered, _currentIndex);
+        photonView.RPC("RPCUpdateCharacter", RpcTarget.AllBuffered, _currentIndex);
+    }
+
+    [PunRPC]
+    void RPCUpdateCharacter(int index)
+    {
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if (i == index)
+                characters[i].SetActive(true);
+            else
+                characters[i].SetActive(false);
+        }
+    }
+
+    [PunRPC]
+    void RPCReady(bool state)
+    {
+        _readySetter(state);
+        checkMark.SetActive(state);
+    }
+
+
 }
