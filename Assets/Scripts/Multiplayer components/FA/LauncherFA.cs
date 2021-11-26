d0dc244ebbb52555899e376e8d1b939f33f5972d
UltraLuke <42553418+UltraLuke@users.noreply.github.com>
2021-11-26 15:03:19 -0300

    arreglando algo

--- Assets/Scripts/Multiplayer components/FA/LauncherFA.cs
+++ Assets/Scripts/Multiplayer components/FA/LauncherFA.cs
@@ -1,9 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class LauncherFA : MonoBehaviour
+public class LauncherFA : MonoBehaviourPunCallbacks
 {
     public MyServer serverPrefab;
-    public ControllerFA controllerFA;
+    public ControllerFA controllerPrefab;
+
+    public void BtcConnect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 4;
+
+        PhotonNetwork.JoinOrCreateRoom("SalaFullAuth", options, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        PhotonNetwork.Instantiate(serverPrefab.name, Vector3.zero, Quaternion.identity);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.Instantiate(controllerPrefab.name, Vector3.zero, Quaternion.identity);
+        }
+    }
 }
